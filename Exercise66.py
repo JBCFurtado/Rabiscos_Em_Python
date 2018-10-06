d41d92c5804f2b396cbbc596c7e33f636cfae05e
Josué Batista <josuebatista.ti@gmail.com>
2018-10-05 21:10:57 -0300

    up

--- Exercise66.py
+++ Exercise66.py
@@ -1,3 +1,15 @@
-#Acrescente zero no inicio da lista
-lista = [1, 2, 10, 5, 20]
-print(lista.index(10))
+# #Acrescente zero no inicio da lista
+# lista = [1, 2, 10, 5, 20]
+# valor = 10
+# resultado = valor in lista
+# print(resultado)
+
+lista9 = [2, 4, 6, 8, 12]
+valor = 10
+resultado = False
+for i in range(len(lista9)):
+    if lista9[i] == valor:
+        resultado = True
+    else:
+        resultado = False
+print(resultado)
